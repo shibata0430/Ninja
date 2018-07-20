fdad24d25dca821ef8ff479a0d98e546ad6e28b7
DESKTOP-AOD3O5V\shibata <rentareo_satomi@yahoo.co.jp>
2018-07-20 19:54:37 +0900

    新たに弾のスクリプトと敵の制御スクリプトを追加

---
+++ Ninja/Ninja/Assets/Script/BulletControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletControl : MonoBehaviour
+{
+    private GameObject playerControl;
+    [SerializeField]
+    private int m_bulletSpeed = 10;
+
+    private Rigidbody2D m_rigidbody2D;
+
+	void Start ()
+    {
+        playerControl = GameObject.FindWithTag("Player");
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
+
+        m_rigidbody2D.velocity = new Vector2(m_bulletSpeed * playerControl.transform.localScale.x, m_rigidbody2D.velocity.y);
+        Vector2 temp = transform.localScale;
+        temp.x = playerControl.transform.localScale.x;
+        transform.localScale = temp;
+    }
+	
+	void Update ()
+    {
+
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+}
